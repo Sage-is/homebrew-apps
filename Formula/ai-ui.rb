11606d0c819ab4b21160bd3eb82d0c01c4ef86c5
Alexander Somma <admin@openco.ca>
2026-04-01 09:43:31 +0000

    Bump version to 1.0.2

--- Formula/ai-ui.rb
+++ Formula/ai-ui.rb
@@ -1,7 +1,7 @@
 class AiUi < Formula
   desc "One-command local deployment of Sage AI UI via Docker"
   homepage "https://github.com/Sage-is/AI-UI"
-  url "https://github.com/Sage-is/homebrew-apps/archive/refs/tags/v1.0.1.tar.gz"
+  url "https://github.com/Sage-is/homebrew-apps/archive/refs/tags/v1.0.2.tar.gz"
   sha256 "d89693efc28c7e0907abdda34e1d9dbe54f5aab482bcb7f395ba661c3c718cec"
   license "MIT"
 
